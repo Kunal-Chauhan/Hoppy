638d3e2b0d0b774fafc648f7ca67bbf3829abff2
Kunal-Chauhan <rismvkc@gmail.com>
2020-09-27 21:46:10 +0530

    Added palyer properties and colours

--- settings.py
+++ settings.py
@@ -4,4 +4,14 @@ height = 650
 title = "Platform Game"
 FPS = 60
 
+# player properties
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = -0.12
+
 # defined colours
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
