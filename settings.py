32af2afa7d23cef95c8c365d0ebf6964e95f831c
Kunal-Chauhan <rismvkc@gmail.com>
2020-10-01 18:54:04 +0530

    Changed player properties

--- settings.py
+++ settings.py
@@ -1,17 +1,17 @@
 # game options and settings
-width = 500
-height = 650
+WIDTH = 500
+HEIGHT = 650
 title = "Platform Game"
 FPS = 60
 
 # player properties
 PLAYER_ACC = 0.8
 PLAYER_FRICTION = -0.12
-PLAYER_GRAV = 0.5
+PLAYER_GRAV = 0.8
 
 # starting platfroms
-PLATFORM_LIST = [(0, height-40, width, 40), (width/2-50, height*3/4, 100, 20),
-                 (125, height-350, 100, 20), (350, 200, 100, 20), (175, 100, 50, 20)]
+PLATFORM_LIST = [(0, HEIGHT-40, WIDTH, 40), (WIDTH/2-50, HEIGHT*3/4, 100, 20),
+                 (125, HEIGHT-350, 100, 20), (350, 200, 100, 20), (175, 100, 50, 20)]
 
 # defined colours
 WHITE = (255, 255, 255)
